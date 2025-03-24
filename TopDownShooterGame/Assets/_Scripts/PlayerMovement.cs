8c3fca686432361a1e009f37925219e4aa3ed911
luttinenjoni <luttinenjoni@gmail.com>
2025-03-24 16:33:33 +0200

    Update PlayerMovement.cs
    Now you can aim your position with mouse.

--- TopDownShooterGame/Assets/_Scripts/PlayerMovement.cs
+++ TopDownShooterGame/Assets/_Scripts/PlayerMovement.cs
@@ -6,18 +6,25 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
 
     public float moveSpeed = 5f;
+    public Camera cam;
 
     Vector2 movement;
+    Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
