b75ea1ad354aede777c62873ce920f6643647aa5
luttinenjoni <luttinenjoni@gmail.com>
2025-03-24 16:18:22 +0200

    Added playermovement
    Simple player movement added with script.

---
+++ TopDownShooterGame/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    public float moveSpeed = 5f;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
